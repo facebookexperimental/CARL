4a17fd0e30a48e422e132c029933bb742c7b061b
syntheticmagus <33846034+syntheticmagus@users.noreply.github.com>
2026-01-15 15:33:26 -0800

    Added rudimentary grabbing.

--- ActionStudio/ImmersiveExperience/source/index.ts
+++ ActionStudio/ImmersiveExperience/source/index.ts
@@ -1,5 +1,45 @@
-import { Engine, FreeCamera, HavokPlugin, HemisphericLight, MeshBuilder, PhysicsAggregate, PhysicsShapeType, Scene, Vector3 } from "@babylonjs/core";
+import { Engine, FreeCamera, HavokPlugin, HemisphericLight, MeshBuilder, Nullable, Observable, Observer, PhysicsAggregate, PhysicsMotionType, PhysicsShapeType, Quaternion, Scene, TransformNode, Vector3, WebXRControllerPointerSelection, WebXRFeatureName, WebXRHand, WebXRHandJoint, WebXRSessionManager } from "@babylonjs/core";
 import HavokPhysics from "@babylonjs/havok";
+
+class HandPinchGrabber extends TransformNode {
+    public onGrabChanged: Observable<boolean> = new Observable<boolean>();
+
+    private _isGrabbing: boolean = false;
+    private _pinchThreshold: number = 0.01;
+    private _frameObserver: Observer<XRFrame>;
+    private _sessionManager: WebXRSessionManager;
+
+    constructor(name: string, hand: WebXRHand, sessionManager: WebXRSessionManager, scene: Scene) {
+        super(name, scene);
+        this.rotationQuaternion = Quaternion.Identity();
+
+        this._sessionManager = sessionManager;
+
+        const indexPosition = new Vector3();
+        const thumbPosition = new Vector3();
+        this._frameObserver = this._sessionManager.onXRFrameObservable.add(() => {
+            indexPosition.copyFrom(hand.getJointMesh(WebXRHandJoint.INDEX_FINGER_TIP)!.absolutePosition);
+            thumbPosition.copyFrom(hand.getJointMesh(WebXRHandJoint.THUMB_TIP)!.absolutePosition);
+
+            this.position.copyFrom(indexPosition);
+            this.position.addInPlace(thumbPosition);
+            this.position.scaleInPlace(0.5);
+
+            if (this._isGrabbing && Vector3.Distance(indexPosition, thumbPosition) > this._pinchThreshold * 1.2) {
+                this._isGrabbing = false;
+                this.onGrabChanged.notifyObservers(this._isGrabbing);
+            } else if (!this._isGrabbing && Vector3.Distance(indexPosition, thumbPosition) <= this._pinchThreshold) {
+                this._isGrabbing = true;
+                this.onGrabChanged.notifyObservers(this._isGrabbing);
+            }
+        });
+    }
+
+    public dispose(doNotRecurse?: boolean, disposeMaterialAndTextures?: boolean): void {
+        super.dispose(doNotRecurse, disposeMaterialAndTextures);
+        this._sessionManager.onXRFrameObservable.remove(this._frameObserver);
+    }
+}
 
 export function initializeImmersiveExperience(canvas: HTMLCanvasElement): void {
     const engine = new Engine(canvas, true);
@@ -13,23 +53,67 @@ export function initializeImmersiveExperience(canvas: HTMLCanvasElement): void {
     });
 
     const scene = new Scene(engine);
-    var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-
-    camera.setTarget(Vector3.Zero());
+    const camera = new FreeCamera("camera1", new Vector3(0, 1, 0), scene);
     camera.attachControl(canvas, true);
 
-    var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
 
-    var sphere = MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
-    sphere.position.y = 4;
-    var ground = MeshBuilder.CreateGround("ground", {width: 10, height: 10}, scene);
+    const ground = MeshBuilder.CreateGround("ground", {width: 10, height: 10}, scene);
+    const table = MeshBuilder.CreateBox("table", {width: 2, depth: 1, height: 0.02}, scene);
+    table.position.set(0, 1, 1);
+    const recording = MeshBuilder.CreateBox("recording", {width: 0.04, depth: 0.04, height: 0.04}, scene);
+    recording.position.set(0, 1.1, 1);
 
+    const recording2 = MeshBuilder.CreateBox("recording2", {width: 0.04, depth: 0.04, height: 0.04}, scene);
+    recording2.position.set(0.5, 1.1, 1);
+    
     HavokPhysics().then((havok) => {
-        var hk = new HavokPlugin(true, havok);
+        const hk = new HavokPlugin(true, havok);
         scene.enablePhysics(new Vector3(0, -9.8, 0), hk);
-        var sphereAggregate = new PhysicsAggregate(sphere, PhysicsShapeType.SPHERE, { mass: 1, restitution:0.75}, scene);
-        var groundAggregate = new PhysicsAggregate(ground, PhysicsShapeType.BOX, { mass: 0 }, scene);
+        
+        const groundAggregate = new PhysicsAggregate(ground, PhysicsShapeType.BOX, { mass: 0 }, scene);
+        const tableAggregate = new PhysicsAggregate(table, PhysicsShapeType.BOX, { mass: 0 }, scene);
+        const recordingAggregate = new PhysicsAggregate(recording, PhysicsShapeType.BOX, { mass: 1 }, scene);
+        recordingAggregate.transformNode.rotationQuaternion = Quaternion.Identity();
+        const recording2Aggregate = new PhysicsAggregate(recording2, PhysicsShapeType.BOX, { mass: 1 }, scene);
+    });
+    
+    scene.createDefaultXRExperienceAsync().then((xr) => {
+        xr.baseExperience.featuresManager.disableFeature(WebXRControllerPointerSelection.Name);
+
+        const handTracking = xr.baseExperience.featuresManager.enableFeature(WebXRFeatureName.HAND_TRACKING, "latest", {
+            xrInput: xr.input,
+            jointMeshes: {
+                sourceMesh: MeshBuilder.CreateBox("jointParent", { size: 1 }),
+                keepOriginalVisible: true,
+            },
+        });
+
+        handTracking.onHandAddedObservable.add((hand) => {
+            const grabber = new HandPinchGrabber("handGrabber", hand, xr.input.xrSessionManager, scene);
+            let observer: Nullable<Observer<XRFrame>> = null;
+            grabber.onGrabChanged.add((isGrabbing) => {
+                if (isGrabbing) {
+                    recording.physicsBody!.setMotionType(PhysicsMotionType.ANIMATED);
+                    recording.physicsBody!.disablePreStep = false;
+                    observer = xr.input.xrSessionManager.onXRFrameObservable.add(() => {
+                        recording.physicsBody!.transformNode.position.copyFrom(grabber.position);
+                    });
+                } else if (!isGrabbing) {
+                    recording.physicsBody!.disablePreStep = true;
+                    recording.physicsBody!.setMotionType(PhysicsMotionType.DYNAMIC);
+                    if (observer) {
+                        xr.input.xrSessionManager.onXRFrameObservable.remove(observer);
+                        observer = null;
+                    }
+                }
+            });
+        });
+
+        handTracking.onHandRemovedObservable.add((hand) => {
+            console.log("Hand removed:", hand);
+        });
     });
 
     engine.runRenderLoop(() => {
